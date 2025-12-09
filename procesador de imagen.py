49d0f6ec2d09526d99c0aab86c62936702b1911f
allisoncano-stack <allison.cano@correounivalle.edu.co>
2025-12-09 11:54:53 -0500

    Agregué definición de botones

--- procesador de imagen.py
+++ procesador de imagen.py
@@ -3,3 +3,13 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
 from PIL import Image
 import numpy as np
+
+
+
+#-------------------------------------------------------------------------------
+# DEFINICIÓN DE BOTONES
+ax_b90   = plt.axes([0.1, 0.15, 0.15, 0.075])
+ax_b180  = plt.axes([0.3, 0.15, 0.15, 0.075])
+ax_b270  = plt.axes([0.5, 0.15, 0.15, 0.075])
+ax_bgris = plt.axes([0.7, 0.15, 0.20, 0.075])
+ax_breset= plt.axes([0.4, 0.05, 0.20, 0.075])
